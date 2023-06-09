fb41e8c3e705f5c4569c5452c94393d2dffaa28e
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-09 11:17:25 +0000

    commit69

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
+#include "HEPTA_EPS.h"
+#include "Hepta9axis.h"
 
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(PA_0,PA_4);
+DigitalOut condition(PB_1);
+Hepta9axis sensor(PB_7,PB_6,0x19,0x69,0x13);
+int main()
+{
+    float mx,my,mz;
+    pc.printf("Magnetometer Mode\r\n");
+    for(int i = 0; i<50; i++) {
+        sensor.sen_mag(&mx,&my,&mz);
+        pc.printf("mag : %f,%f,%f\r\n",mx,my,mz);
+        wait(1.0);
+    }
 }
